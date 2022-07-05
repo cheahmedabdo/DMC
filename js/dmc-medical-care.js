b91d828e5e7976c7e7eb9acf0f56fd412aa4a5be
devahmedabdo <devahmedabdo@gmail.com>
2022-07-05 18:33:17 +0200

    reduce js code

--- js/dmc-medical-care.js
+++ js/dmc-medical-care.js
@@ -3,8 +3,6 @@ window.onload = function () {
   onloadPage.classList.add("loadpageHiden");
   setTimeout(function () {
     document.querySelector(".mCareLandingContnet").classList.add("reveal");
-  }, 1500);
-  setTimeout(function () {
     document.querySelector(".mCareLandingH1").classList.add("reveal");
   }, 1500);
 };
